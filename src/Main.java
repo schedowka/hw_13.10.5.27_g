e3ccef5162f0d485172e21e51ae10d5d1b2e7edc
schedowka <marina_molodkina@proton.me>
2024-10-15 19:42:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введи а: введи б: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+
+        int sum=0;
+        for (int i=a; i<=b; i++)
+        sum +=i;
+        System.out.println(sum);
+    }
+}
